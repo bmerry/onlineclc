190aa10102214c560c1d23e74fd75a0bb5e1809b
Bruce Merry <bmerry@gmail.com>
2011-05-30 10:58:09 +0200

    Make onlineclc functional

--- src/onlineclc.c
+++ src/onlineclc.c
@@ -21,6 +21,9 @@
 #ifndef _POSIX_C_SOURCE
 # define _POSIX_C_SOURCE 200112L
 #endif
+#ifndef _XOPEN_SOURCE
+# define _XOPEN_SOURCE 500
+#endif
 
 #if HAVE_CONFIG_H
 # include <config.h>
@@ -36,9 +39,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
-
-static const char *machine = NULL;
-static const char *output_filename = NULL;
+#include <assert.h>
 
 typedef struct
 {
@@ -76,6 +77,7 @@ static void pdie(int exitcode, const char *msg, ...)
     va_start(ap, msg);
     vfprintf(stderr, msg, ap);
     va_end(ap);
+    fputs(": ", stderr);
     perror(NULL);
     exit(exitcode);
 }
@@ -91,13 +93,13 @@ static const char *error_to_string(cl_int error)
         ERROR_CASE(CL_INVALID_BUILD_OPTIONS);
         ERROR_CASE(CL_INVALID_DEVICE);
         ERROR_CASE(CL_INVALID_DEVICE_TYPE);
-        ERROR_CASE(CL_INVALID_DEVICE_LIST);
         ERROR_CASE(CL_INVALID_OPERATION);
+        ERROR_CASE(CL_INVALID_PLATFORM);
         ERROR_CASE(CL_INVALID_PROGRAM);
         ERROR_CASE(CL_INVALID_VALUE);
         ERROR_CASE(CL_OUT_OF_HOST_MEMORY);
     default:
-        return "(unknown error)";
+        return "unknown error";
     }
 #undef ERROR_CASE
 }
@@ -114,65 +116,98 @@ static void die_cl(cl_int status, int exitcode, const char *msg, ...)
     exit(exitcode);
 }
 
-static cl_device find_device(const char *device_name)
+static cl_device_id find_device(const char *device_name)
 {
-    cl_int status;
-    cl_uint num_devices, i;
-    cl_device_id *devices;
     size_t size;
-    char *current_device_name;
+    cl_int status;
 
-    /* Get number of available devices */
-    status = clGetDeviceIDs(CL_DEVICE_TYPE_ALL, 0, NULL, &num_devices);
+    cl_uint num_platforms, i;
+    cl_platform_id *platforms;
+
+    cl_device_id ans = NULL;
+    cl_uint total_devices = 0;
+    cl_uint match_devices = 0;
+
+    /* Get number of available platforms */
+    status = clGetPlatformIDs(0, NULL, &num_platforms);
     if (status != CL_SUCCESS)
-        die_cl(status, 1, "Failed to get device ID count");
-    if (num_devices == 0)
-        die(1, "No OpenCL devices found");
+        die_cl(status, 1, "Failed to get platform ID count");
+    if (num_platforms == 0)
+        die_cl(status, 1, "No OpenCL platforms found");
 
-    size = sizeof(cl_device_id) * num_devices;
-    devices = (cl_device_id *) malloc(size);
-    if (devices == NULL)
-        die("Could not allocate %zu bytes for device IDs", size);
-    status = clGetDeviceIDs(CL_DEVICE_TYPE_ALL, num_devices, devices, NULL);
+    size = sizeof(cl_platform_id) * num_platforms;
+    platforms = (cl_platform_id *) malloc(size);
+    if (platforms == NULL)
+        die(1, "Could not allocate %zu bytes for platform IDs", size);
+    status = clGetPlatformIDs(num_platforms, platforms, NULL);
     if (status != CL_SUCCESS)
-        die_cl(status, 1, "Failed to get device IDs");
+        die_cl(status, 1, "Failed to get platform IDs");
 
-    for (i = 0; i < num_devices; i++)
+    for (i = 0; i < num_platforms; i++)
     {
-        size_t name_len;
-        char *name;
-        status = clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 0, NULL, &name_len);
-        if (status != CL_SUCCESS)
-            die_cl(status, 1, "Failed to query device name length");
-        name = (char *) malloc(name_len);
-        if (name == NULL)
-            die("Could not allocate %zu bytes for device name", name_len);
-        status = clGetDeviceInfo(devices[i], CL_DEVICE_NAME, name_len, name, NULL);
-        if (status != CL_SUCCESS)
-            die_cl(status, 1, "Failed to query device name");
+        cl_device_id *devices;
+        cl_uint num_devices, j;
 
-        if (device_name == NULL || strcmp(name, device_name) == 0)
+        /* Get number of available devices */
+        status = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, NULL, &num_devices);
+        if (status != CL_SUCCESS)
+            die_cl(status, 1, "Failed to get device ID count");
+        total_devices += num_devices;
+        if (num_devices == 0)
+            continue;
+
+        size = sizeof(cl_device_id) * num_devices;
+        devices = (cl_device_id *) malloc(size);
+        if (devices == NULL)
+            die(1, "Could not allocate %zu bytes for device IDs", size);
+        status = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, num_devices, devices, NULL);
+        if (status != CL_SUCCESS)
+            die_cl(status, 1, "Failed to get device IDs");
+
+        for (j = 0; j < num_devices; j++)
         {
-            /* Match found */
-            /* TODO: check that the device supports online compilation */
-            cl_device_id ans = device_ids[i];
+            size_t name_len;
+            char *name;
+            status = clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, NULL, &name_len);
+            if (status != CL_SUCCESS)
+                die_cl(status, 1, "Failed to query device name length");
+            name = (char *) malloc(name_len);
+            if (name == NULL)
+                die(1, "Could not allocate %zu bytes for device name", name_len);
+            status = clGetDeviceInfo(devices[j], CL_DEVICE_NAME, name_len, name, NULL);
+            if (status != CL_SUCCESS)
+                die_cl(status, 1, "Failed to query device name");
 
-            if (device_name == NULL && num_devices > 1)
+            if (device_name == NULL || strcmp(name, device_name) == 0)
             {
-                fprintf(stderr, "Warning: multiple devices found. Defaulting to `%s'\n", name);
+                /* Match found */
+                /* TODO: check that the device supports online compilation */
+                if (match_devices == 0)
+                    ans = devices[j];
+                match_devices++;
             }
             free(name);
-            free(device_ids);
-            return ans;
         }
+        free(devices);
+    }
+    free(platforms);
+
+    if (total_devices == 0)
+        die(1, "No OpenCL devices found");
+    else if (match_devices == 0)
+    {
+        assert(device_name != NULL);
+        die(1, "No OpenCL device called `%s' found", device_name);
     }
 
-    free(device_ids);
-    die(1, "No OpenCL device called `%s' found", device_name);
-    return 0;
+    if (match_devices > 1)
+    {
+        fprintf(stderr, "Warning: multiple devices match, using the first one\n");
+    }
+    return ans;
 }
 
-static cl_context create_context(cl_device device)
+static cl_context create_context(cl_device_id device)
 {
     cl_context ctx;
     cl_int status;
@@ -183,14 +218,44 @@ static cl_context create_context(cl_device device)
     return ctx;
 }
 
+static void dump_build_log(FILE *out, cl_program program, cl_device_id device)
+{
+    cl_int status;
+    char *build_log;
+    size_t len;
+
+    status = clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &len);
+    if (status != CL_SUCCESS)
+        die_cl(status, 1, "Failed to get length of build log");
+    if (len == 0)
+        return;
+
+    build_log = (char *) malloc(len * sizeof(char));
+    if (build_log == NULL)
+        die(1, "Failed to allocate %zu bytes for the build log", len);
+
+    status = clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, len, build_log, NULL);
+    if (status != CL_SUCCESS)
+        die_cl(status, 1, "Failed to get build log");
+    /* The CL implementation should null-terminate itself; this is just to
+     * protect against bugs.
+     */
+    build_log[len - 1] = '\0';
+
+    fputs(build_log, out);
+    free(build_log);
+}
+
 static cl_program create_program(
     cl_context ctx,
-    cl_device device,
+    cl_device_id device,
     const char *source_filename,
     const char *options)
 {
-    char *src;
-    struct stat fb;
+    void *addr;
+    const char *srcs[4];
+    size_t src_lens[4];
+    struct stat sb;
     cl_int status;
     int fd;
     size_t len;
@@ -204,15 +269,30 @@ static cl_program create_program(
         pdie(1, "Failed to stat `%s'", source_filename);
 
     len = sb.st_size;
-    src = (char *) mmap(NULL, len, PROT_READ, fd, 0);
-    if (addr == NULL)
-        pdie(1, "Failed to map `%s'", source_filename);
+    if (len == 0)
+    {
+        /* Can't portably mmap 0 bytes */
+        addr = "";
+    }
+    else
+    {
+        addr = mmap(NULL, len, PROT_READ, MAP_SHARED, fd, 0);
+        if (addr == MAP_FAILED)
+            pdie(1, "Failed to map `%s'", source_filename);
+    }
 
-    program = clCreateProgramWithSource(ctx, 1, &src, &len, &status);
+    srcs[0] = "#line 1 \"";        src_lens[0] = 0;
+    /* TODO: escape quotes in the filename */
+    srcs[1] = source_filename;     src_lens[1] = 0;
+    srcs[2] = "\"\n";              src_lens[2] = 0;
+    srcs[3] = (const char *) addr; src_lens[3] = len;
+
+    program = clCreateProgramWithSource(ctx, 4, srcs, src_lens, &status);
     if (status != CL_SUCCESS)
         die_cl(status, 1, "Failed to load source from `%s'", source_filename);
 
-    munmap(src, sb.st_size);
+    if (len != 0)
+        munmap(addr, sb.st_size);
     close(fd);
 
     if (options == NULL)
@@ -223,6 +303,8 @@ static cl_program create_program(
     case CL_SUCCESS:
         break;
     case CL_BUILD_PROGRAM_FAILURE:
+        dump_build_log(stderr, program, device);
+        exit(1);
     default:
         die_cl(status, 1, "Failed to build `%s'", source_filename);
     }
@@ -296,18 +378,18 @@ static void process_options(compiler_options *options, int argc, const char * co
         {
             if (i == argc - 2)
                 usage(2, "Source file not specified");
-            if (machine != NULL)
+            if (options->machine != NULL)
                 die(2, "-b option specified twice");
-            machine = argv[i + 1];
+            options->machine = argv[i + 1];
             i++;
         }
         else if (0 == strcmp(argv[i], "-o"))
         {
             if (i == argc - 2)
                 usage(2, "Source file not specified");
-            if (output_filename != NULL)
+            if (options->output_filename != NULL)
                 die(2, "-o option specified twice");
-            output_filename = argv[i + 1];
+            options->output_filename = argv[i + 1];
             i++;
         }
         else if (0 == strcmp(argv[i], "--help"))
@@ -334,6 +416,47 @@ static void process_options(compiler_options *options, int argc, const char * co
     }
 }
 
+static void write_program(const char *output_filename, cl_program program, cl_device_id device)
+{
+    cl_int status;
+    cl_uint num_devices;
+    size_t sizes[1];
+    unsigned char *binaries[1];
+    int fd;
+
+    /* Verify that there is only one device */
+    status = clGetProgramInfo(program, CL_PROGRAM_NUM_DEVICES, sizeof(cl_uint), &num_devices, NULL);
+    if (status != CL_SUCCESS)
+        die_cl(status, 1, "Failed to query number of devices from program");
+    if (num_devices != 1)
+        die(1, "Expected one device but found %u", (unsigned int) num_devices);
+
+    status = clGetProgramInfo(program, CL_PROGRAM_BINARY_SIZES, sizeof(size_t), sizes, NULL);
+    if (status != CL_SUCCESS)
+        die_cl(status, 1, "Failed to obtain binary size");
+
+    if (sizes[0] == 0)
+        die(1, "No binary was produced by the compiler");
+
+    fd = open(output_filename, O_RDWR | O_CREAT, 0666);
+    if (fd < 0)
+        pdie(1, "Could not open `%s'", output_filename);
+
+    if (ftruncate(fd, sizes[0]) != 0)
+        pdie(1, "Could not resize `%s' to %zu bytes", output_filename, sizes[0]);
+
+    binaries[0] = (unsigned char *) mmap(NULL, sizes[0], PROT_WRITE, MAP_SHARED, fd, 0);
+    if (binaries[0] == MAP_FAILED)
+        pdie(1, "Could not map `%s'", output_filename);
+
+    status = clGetProgramInfo(program, CL_PROGRAM_BINARIES, sizeof(unsigned char *), binaries, NULL);
+    if (status != CL_SUCCESS)
+        die_cl(status, 1, "Failed to query the program binary");
+
+    munmap((void *) binaries[0], sizes[0]);
+    close(fd);
+}
+
 int main(int argc, const char * const *argv)
 {
     compiler_options options;
@@ -343,6 +466,12 @@ int main(int argc, const char * const *argv)
     s.device = find_device(options.machine);
     s.ctx = create_context(s.device);
     s.program = create_program(s.ctx, s.device, options.source_filename, options.options);
+    dump_build_log(stderr, s.program, s.device);
+    if (options.output_filename != NULL)
+        write_program(options.output_filename, s.program, s.device);
+
+    clReleaseProgram(s.program);
+    clReleaseContext(s.ctx);
 
     return 0;
 }
--- wscript
+++ wscript
@@ -3,9 +3,14 @@ out = 'build'
 
 def options(opt):
     opt.load('compiler_c')
+    opt.add_option('--cl-headers', action='store', default=None, help='Include path for OpenCL')
 
 def configure(conf):
     conf.load('compiler_c')
+    conf.env.append_value('LIB_OPENCL', ['OpenCL'])
+    if conf.options.cl_headers:
+        conf.env.append_value('INCLUDES_OPENCL', [conf.options.cl_headers])
+    conf.check_cc(header_name = 'CL/cl.h', use = 'OPENCL')
 
 def build(bld):
     bld.recurse('src')
